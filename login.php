aea9baa8fa86ac45c12a86f9330dc8fc599a62ac
Jonathan Antivo <Jonathan@GAPhilippines3s-Mac-mini.local>
2012-12-14 15:26:03 +0800

    dsfsdf

--- login.php
+++ login.php
@@ -21,10 +21,12 @@
 			<h2>Login</h2>
 			<form class="form-horizontal" action = "MVC/controller/loginUser.php" method = "post">
 			<form action="/dashboard.php" class="form-horizontal">
+				<?php if(isset($_GET['err'])): ?>
 				<div class="alert alert-error fade in">
 				  <button data-dismiss="alert" class="close" type="button">×</button>
 				  <strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.
 				</div>
+				<?php endif; ?>
 				<div class="control-group">
 					<label class="control-label" for="inputEmail">Email</label>
 					<div class="controls">
