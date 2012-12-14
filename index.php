69175ee756473e9fe7868a711904b81a40055d51
Adelon Zeta <adelonzeta@gmail.com>
2012-12-14 10:26:25 +0800

    First Official Commit: added bootstrap files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Miss U App</title>
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/style.css" />
+	
+	<!-- Google Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Rufina' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	
+	
+	<!-- Javascript -->
+	<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
